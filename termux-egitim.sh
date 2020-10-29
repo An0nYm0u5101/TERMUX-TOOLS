5f00d5b87915f5e437edbcdbe7b0304d2dfc590a
u0_a221 <u0_a221@localhost>
2020-10-29 20:26:04 +0300

    güncelleme

--- termux-egitim.sh
+++ termux-egitim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 clear
+if [[ $1 == güncelle ]];then
+	cd files
+	bash güncelleme.sh güncelle
+	exit
+fi
 hatakontrol=$(curl -s https://github.com/termux-egitim |wc -l)
 if [[ $hatakontrol == 0 ]];then
 	echo
@@ -36,91 +41,14 @@ if [[ $1 == kısayol || $1 == k || $1 == K ]];then
 	echo
 	echo
 	echo
-	printf "\e[32m[✓] tools-termux-egitim}e[0m KISAYOL OLUŞTURULDU"
+	printf "\e[32m[✓] tools-termux-egitim\e[0m KISAYOL OLUŞTURULDU"
 	echo
 	echo
 	echo
 	exit
-fi
-kucukad=$(sed -n 1p README.md)
-buyukad=$(sed -n 2p README.md)
-#################### GÜNCELLEME TARİHİ EKLEME ####################
-if [[ $1 == güncelle || $1 == güncelleme ]];then
-	echo
-	echo
-	echo
-	printf "\e[33mSON GÜNCELLEME TARİHİ \e[31m>\e[0m $(sed -n 3p README.md |tr -d \"Güncelleme\")"
-	echo
-	echo
-	echo
-	history -s $(date +%d.%m.%G)
-	history -s $(sed -n 3p README.md |tr -d "Güncelleme")
-	read -e -p $'\e[32mTARİH GİRİNİZ \e[31m>\e[0m ' tarih
-	echo
-	echo
-	songuncelleme=$(sed -n 3p README.md |tr -d "Güncelleme ")
-	sed -ie "s/$songuncelleme/$tarih/g" $kucukad.sh
-	songuncelleme2=$(sed -n 3p README.md |tr -d "Güncelleme ")
-	sed -ie "s/$songuncelleme2/$tarih/g" README.md
-	echo
-	echo
-	echo
-	printf "\e[32m[*]\e[0m TARİH GÜNCELLENDİ "
-	echo
-	echo
-	if [[ -a $kucukad.she ]];then
-		rm $kucukad.she
-	fi
-	if [[ -a README.mde ]];then
-		rm README.mde
-	fi
-	exit
-
-fi
-#################### OTOMATİK GÜNCEKLEME ####################
-readme=$(sed -n 3p README.md |tr -d "Güncelleme ")
-guncelleme=$(curl -s "https://github.com/termux-egitim/$buyukad" |grep -o $readme)
-if [ "$guncelleme" = "$readme" ];then
-	echo
-else
-	kontrol=$(curl -s https://github.com/termux-egitim/$kucukad |grep -o not-found |wc -w)
-	if [[ $kontrol == 0 ]];then
-		echo
-	else
-		echo
-		echo
-		echo
-		printf "\e[31m[!]\e[0m$buyukad GÜNCELLEME YAPILAMIYOR \e[31m!!!\e[0m"
-		echo
-		echo
-		echo
-		echo
-		sleep 2
-		printf "\e[31m[!]\e[0m $buyukad DEPOSU BULUNAMADI \e[31m!!!\e[0m"
-		echo
-		echo
-		exit
-	fi
-
-	echo
-	echo
-	echo
-	printf "\e[32m[*]\e[0m $buyukad GÜNCELLENİYOR "
-	echo
-	echo
-	echo
-	sleep 2
-	rm -rf *
-	rm -rf .git
-	git clone https://github.com/termux-egitim/$kucukad
-	cd $kucukad
-	mv * ../
-	mv .git ../
-	cd ..
-	rm -rf $kucukad
-	bash $kucukad.sh
 fi
 cd files
+bash güncelleme.sh
 curl -s "https://github.com/termux-egitim?tab=repositories" |grep "<a href=\"/termux-egitim/" > tool.txt
 sed -ie "s/<a href=\"\/termux-egitim\///g" tool.txt
 sed -ie "s/\" itemprop=\"name codeRepository\" >//g" tool.txt
@@ -203,3 +131,4 @@ echo
 git clone https://github.com/termux-egitim/$(sed -n $sec\p tools.txt)
 mv $(sed -n $sec\p tools.txt) $HOME
 rm tool.txt tools.txt tool.txte
+
